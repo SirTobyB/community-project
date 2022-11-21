461973217de77c4e4e8f7e6a594d8c6983c0d09a
Manuel Rauber <manuel.rauber@boundfoxstudios.com>
2022-11-21 19:43:55 +0100

    refactor(terrain): remove unused code

--- CommunityProject/Assets/_Game/Scripts/Runtime/Terrain/Tiles/IntBounds.cs
+++ CommunityProject/Assets/_Game/Scripts/Runtime/Terrain/Tiles/IntBounds.cs
@@ -24,21 +24,6 @@ namespace BoundfoxStudios.CommunityProject.Terrain.Tiles
 			Center = min + Size / 2;
 		}
 
-		/*
-		public IntBounds Intersection(IntBounds other) =>
-			new(
-				new(math.max(Min.x, other.Min.x), math.max(Min.y, other.Min.y)),
-				new(math.min(Max.x, other.Max.x), math.min(Max.y, other.Max.y))
-			);
-
-		public bool Intersects(IntBounds other) =>
-			Min.x <= other.Max.x
-			&& Max.x >= other.Min.x
-			&& Min.y <= other.Max.y
-			&& Max.y >= other.Min.y;
-
-		public bool Contains(IntBounds other) => math.all(other.Min >= Min) && math.all(other.Max <= Max);*/
-
 		public override string ToString() => $"IntBounds [Min ({Min}) Max({Max})]";
 
 		public readonly bool Contains(int2 position) =>
